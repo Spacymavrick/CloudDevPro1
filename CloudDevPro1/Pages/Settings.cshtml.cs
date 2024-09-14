28c9b91065e3b4efc07967a9f24fec4a4bbde2e9
Spacymavrick <96030302+Spacymavrick@users.noreply.github.com>
2024-09-14 17:44:56 +0200

    Update

---
+++ CloudDevPro1/Pages/Settings.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CloudDevPro1.Pages
+{
+    public class SettingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
